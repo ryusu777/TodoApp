d74bdbb08e92032b9e42c2ed58b9b79e3afef7be
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-04-15 23:36:11 +0700

    feat: implemented subdomain persistor handler

---
+++ src/ProjectManagement/ProjectManagement.Infrastructure/Subdomain/SubdomainPersistHandler.cs
@@ -0,0 +1,52 @@
+﻿using ProjectManagement.Application.Subdomain.Events;
+using ProjectManagement.Domain.Subdomain.Events;
+using ProjectManagement.Infrastructure.Persistence;
+using ProjectManagement.Infrastructure.Persistence.Data;
+
+namespace ProjectManagement.Infrastructure.Subdomain;
+
+public class SubdomainPersistHandler :
+    IPersistEventHandler<SubdomainCreated>,
+    IPersistEventHandler<SubdomainDeleted>,
+    IPersistEventHandler<SubdomainUpdated>,
+    IPersistEventHandler<SubdomainKnowledgeCreated>,
+    IPersistEventHandler<SubdomainKnowledgeDeleted>,
+    IPersistEventHandler<SubdomainKnowledgeUpdated>
+{
+    public Task Handle(SubdomainKnowledgeUpdated notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(SubdomainKnowledgeDeleted notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        dbContext.SubdomainKnowledges.Remove(notification.SubdomainKnowledge);
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(SubdomainKnowledgeCreated notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        dbContext.SubdomainKnowledges.Add(notification.SubdomainKnowledge);
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(SubdomainUpdated notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(SubdomainDeleted notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        dbContext
+            .Subdomains
+            .Remove(notification.Subdomain);
+
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(SubdomainCreated notification, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        dbContext.Subdomains.Add(notification.Subdomain);
+        return Task.CompletedTask;
+    }
+}
