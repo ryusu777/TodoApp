6fb46e2291de00636535ce2684cb66ae06424163
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-01 15:57:54 +0700

    fix AuthContext: changed JwtOptions to be class instead of recrod

--- src/AuthContext/AuthContext.Application/Identity/Model/JwtOptions.cs
+++ src/AuthContext/AuthContext.Application/Identity/Model/JwtOptions.cs
@@ -1,9 +1,11 @@
 namespace AuthContext.Application.Identity.Model;
 
-public record JwtOptions(
-    string Issuer,
-    string Audience,
-    string SecretKey,
-    int JwtExpiresInMinutes,
-    int RefreshTokenExpiresInDays
-);
+public class JwtOptions
+{
+    public const string OptionSection = "JwtOptions";
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required string SecretKey { get; set; }
+    public required int JwtExpiresInMinutes { get; set; }
+    public required int RefreshTokenExpiresInDays { get; set; }
+}
