169200c71ee095f144b5c41e72b4cb86107d8876
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-01 16:01:59 +0700

    feat IntegrationContext: configured program.cs

--- src/IntegrationContext/IntegrationContext.Presentation/Program.cs
+++ src/IntegrationContext/IntegrationContext.Presentation/Program.cs
@@ -1,9 +1,14 @@
+using IntegrationContext.Infrastructure;
+using IntegrationContext.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
