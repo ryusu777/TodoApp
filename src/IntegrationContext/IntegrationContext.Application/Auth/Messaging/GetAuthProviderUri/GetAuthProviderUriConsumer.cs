ae0dedec21b1241b7e2ca75183a18a5db3bea121
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-05 00:40:28 +0700

    feat IntegrationContext: implemented GetAuthProviderUriConsumer

--- src/IntegrationContext/IntegrationContext.Application/Auth/Messaging/GetAuthProviderUri/GetAuthProviderUriConsumer.cs
+++ src/IntegrationContext/IntegrationContext.Application/Auth/Messaging/GetAuthProviderUri/GetAuthProviderUriConsumer.cs
@@ -1,46 +1,24 @@
-using IntegrationContext.Application.Auth.Models;
+using IntegrationContext.Application.Auth.Queries.GetAuthProviderUri;
 using MassTransit;
 using MassTransitContracts.GetAuthProviderUri;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Http;
+using MediatR;
 
 namespace IntegrationContext.Application.Auth.Messaging.GetAuthProviderUri;
 
 public class GetAuthProviderUriConsumer : IConsumer<GetAuthProviderUriRequest>
 {
-    private readonly string _giteaUrl;
-    private readonly GiteaClientCredentials _credentials;
-    private readonly string _clientUrl;
-    private string BaseUrl => $"{_clientUrl}/authorize-gitea";
-
-    public GetAuthProviderUriConsumer(IConfiguration config, IOptions<GiteaClientCredentials> credentials)
+    private readonly ISender _sender;
+    public GetAuthProviderUriConsumer(ISender sender)
     {
-        var url = config["GiteaUrl"];
-        if (url is null)
-            throw new Exception("Gitea url is not found, please set Gitea Url in configuration");
-
-        var clientUrl = config["ClientUrl"];
-        if (clientUrl is null)
-            throw new Exception("Client url is not found, please set Client Url in configuration");
-
-        _clientUrl = clientUrl;
-        _giteaUrl = url;
-        _credentials = credentials.Value;
+        _sender = sender;
     }
 
     public async Task Consume(ConsumeContext<GetAuthProviderUriRequest> context)
     {
-        var uriBuilder = new UriBuilder(_giteaUrl);
-        uriBuilder.Path = "/login/oauth/authorize";
-        uriBuilder.Query = 
-            "client_id=" + _credentials.ClientId + "&" +
-            "redirect_uri=" + BaseUrl + "&" +
-            "response_type=code" + "&" +
-            "state=" + context.Message.State
-            ;
+        var result = await _sender
+            .Send(new GetAuthProviderUriQuery(context.Message.State));
 
-        await context.RespondAsync(new GetAuthProviderUriResponse(uriBuilder.Uri));
+        await context.RespondAsync(result.Value!);
     }
 }
 
