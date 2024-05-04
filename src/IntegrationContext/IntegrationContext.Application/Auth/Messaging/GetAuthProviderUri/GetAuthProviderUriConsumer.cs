b3c83f8b313d47bf1fcc2d1c42f69b5022dcf86c
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-05 01:41:32 +0700

    refactor IntegrationContext: moved consumer logic to its consumer class instead of calling query

--- src/IntegrationContext/IntegrationContext.Application/Auth/Messaging/GetAuthProviderUri/GetAuthProviderUriConsumer.cs
+++ src/IntegrationContext/IntegrationContext.Application/Auth/Messaging/GetAuthProviderUri/GetAuthProviderUriConsumer.cs
@@ -1,24 +1,45 @@
-using IntegrationContext.Application.Auth.Queries.GetAuthProviderUri;
+using IntegrationContext.Application.Auth.Models;
 using MassTransit;
 using MassTransitContracts.GetAuthProviderUri;
-using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace IntegrationContext.Application.Auth.Messaging.GetAuthProviderUri;
 
 public class GetAuthProviderUriConsumer : IConsumer<GetAuthProviderUriRequest>
 {
-    private readonly ISender _sender;
-    public GetAuthProviderUriConsumer(ISender sender)
+    private readonly string _giteaUrl;
+    private readonly GiteaClientCredentials _credentials;
+    private readonly string _clientUrl;
+    private string BaseUrl => $"{_clientUrl}/authorize-gitea";
+
+    public GetAuthProviderUriConsumer(IConfiguration config, IOptions<GiteaClientCredentials> credentials)
     {
-        _sender = sender;
+        var url = config["GiteaUrl"];
+        if (url is null)
+            throw new Exception("Gitea url is not found, please set Gitea Url in configuration");
+
+        var clientUrl = config["ClientUrl"];
+        if (clientUrl is null)
+            throw new Exception("Client url is not found, please set Client Url in configuration");
+
+        _clientUrl = clientUrl;
+        _giteaUrl = url;
+        _credentials = credentials.Value;
     }
 
     public async Task Consume(ConsumeContext<GetAuthProviderUriRequest> context)
     {
-        var result = await _sender
-            .Send(new GetAuthProviderUriQuery(context.Message.State));
+        var uriBuilder = new UriBuilder(_giteaUrl);
+        uriBuilder.Path = "/login/oauth/authorize";
+        uriBuilder.Query = 
+            "client_id=" + _credentials.ClientId + "&" +
+            "redirect_uri=" + BaseUrl + "&" +
+            "response_type=code" + "&" +
+            "state=" + context.Message.State
+            ;
 
-        await context.RespondAsync(result.Value!);
+        await context.RespondAsync(new GetAuthProviderUriResponse(uriBuilder.Uri));
     }
 }
 
--- src/IntegrationContext/IntegrationContext.Application/Auth/Queries/GetAuthProviderUri/GetAuthProviderUriQuery.cs
+++
@@ -1,6 +0,0 @@
-using IntegrationContext.Application.Abstractions.Messaging;
-using MassTransitContracts.GetAuthProviderUri;
-
-namespace IntegrationContext.Application.Auth.Queries.GetAuthProviderUri;
-
-public record GetAuthProviderUriQuery(string State) : IQuery<GetAuthProviderUriResponse>;
