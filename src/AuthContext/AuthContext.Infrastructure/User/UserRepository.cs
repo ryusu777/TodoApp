66b7cbbec39730a5e4f6d44d7f2836365737c1a5
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-06 03:38:45 +0700

    feat AuthContext: implemented user repository get user by username

--- src/AuthContext/AuthContext.Infrastructure/User/UserRepository.cs
+++ src/AuthContext/AuthContext.Infrastructure/User/UserRepository.cs
@@ -1,17 +1,33 @@
 using AuthContext.Application.User;
+using AuthContext.Domain.User;
+using AuthContext.Domain.User.ValueObjects;
+using AuthContext.Infrastructure.Persistence.Data;
 using Library.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthContext.Infrastructure.User;
 
 public class UserRepository : IUserRepository
 {
-    public Task<Result<Domain.User.User>> GetUserByUsernameAsync(string username, CancellationToken ct)
+    private readonly AppDbContext _dbContext;
+
+    public UserRepository(AppDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
     }
 
-    public Task<Result> RegisterUserAsync(string username, CancellationToken ct)
+    public async Task<Result<Domain.User.User>> GetUserByUsernameAsync(string username, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var result = await _dbContext.Users.FirstOrDefaultAsync(e => e.UserName == username, ct);
+        if (result is null)
+            return Result.Failure<Domain.User.User>(UserDomainError.UserNotFound);
+
+        return Result.Success(
+            Domain.User.User.Create(
+                UserId.Create(result.UserName!),
+                Domain.User.ValueObjects.Email.Create(result.Email!),
+                result.RefreshToken
+            )
+        );
     }
 }
