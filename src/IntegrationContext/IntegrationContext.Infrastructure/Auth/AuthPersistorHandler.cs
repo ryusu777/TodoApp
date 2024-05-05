cfc18b0a0431fd4d5931832f51f5484bf33d1929
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-06 03:41:21 +0700

    feat IntegrationContext: implemented GiteaUserCreated persistor handler

---
+++ src/IntegrationContext/IntegrationContext.Infrastructure/Auth/AuthPersistorHandler.cs
@@ -0,0 +1,16 @@
+﻿using IntegrationContext.Domain.Auth;
+using IntegrationContext.Domain.Auth.Events;
+using IntegrationContext.Infrastructure.Persistence;
+using IntegrationContext.Infrastructure.Persistence.Data;
+
+namespace IntegrationContext.Infrastructure.Auth;
+
+public class AuthPersistorHandler : IPersistEventHandler<GiteaUserCreated>
+{
+	public Task Handle(GiteaUserCreated notification, AppDbContext dbContext, CancellationToken cancellationToken)
+	{
+		dbContext.GiteaUsers.Add(notification.User);
+
+		return Task.CompletedTask;
+	}
+}
