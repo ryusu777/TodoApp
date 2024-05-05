9afb503bdac7eae91068e8180948c147a9a496a7
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-06 03:42:31 +0700

    migration IntegrationContext: added UserChanges migration (inital)

---
+++ src/IntegrationContext/IntegrationContext.Infrastructure/Migrations/20240505181213_UserChanges.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegrationContext.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "integration");
+
+            migrationBuilder.CreateTable(
+                name: "GiteaUser",
+                schema: "integration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JwtToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JwtExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RefreshTokenExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GiteaUser", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GiteaUser",
+                schema: "integration");
+        }
+    }
+}
