d51b235fdd29ccdcd171f27a2c58ed8cbb8ac6b2
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-06 03:40:21 +0700

    migration AuthContext: added UserChanges migration (Initial)

---
+++ src/AuthContext/AuthContext.Infrastructure/Migrations/20240505181256_UserChanges.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthContext.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GiteaUserId",
+                schema: "auth",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GiteaUserId",
+                schema: "auth",
+                table: "User",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
