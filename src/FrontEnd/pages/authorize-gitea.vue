34f1d00ae88cb0b0cdd55c704944be0737221679
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-06 03:45:30 +0700

    feat Frontend: implemented authorize-gitea page for handling in middleware

---
+++ src/FrontEnd/pages/authorize-gitea.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: false,
+  middleware: 'authorize-gitea'
+});
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <h1>If you see this page, something went wrong when authorizing</h1>
+  </div>
+</template>
