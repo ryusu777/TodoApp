126162c986ea7498b594ae6b3e5e0dea51362000
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-07 00:58:03 +0700

    feat FrontEnd: implemented authAPi for SignIn and OnboardUser

--- src/FrontEnd/domain/auth/api/authApi.ts
+++ src/FrontEnd/domain/auth/api/authApi.ts
@@ -1,6 +1,26 @@
 export const AuthApiRoute = {
+  SignIn: "/auth/sign-in",
   SignInWithGitea: "/auth/sign-in-gitea",
-  AuthorizeGitea: "/auth/authorize-gitea"
+  AuthorizeGitea: "/auth/authorize-gitea",
+  OnboardUser: "/auth/onboard-user"
+}
+
+type SignInRequest = {
+  username: string;
+  password: string;
+}
+
+type SignInResponse = {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  refresh_token: string;
+}
+
+export function SignIn(payload: SignInRequest) {
+  const api = useApi();
+  return api
+    .$post<SignInResponse>(AuthApiRoute.SignIn, payload);
 }
 
 type SignInWithGiteaResponse = string;
@@ -32,4 +52,16 @@ export function AuthorizeGitea(authorizationCode: string) {
   const api = useApi();
   return api
     .$post<AuthorizeGiteaResponse>(AuthApiRoute.AuthorizeGitea, { authorizationCode });
+}
+
+type OnboardUserRequest = {
+  username: string;
+  email: string;
+  password: string;
+  changePasswordCode: string;
+};
+
+export function OnboardUser(payload: OnboardUserRequest) {
+  const api = useApi();
+  return api.$post(AuthApiRoute.OnboardUser, payload);
 }
