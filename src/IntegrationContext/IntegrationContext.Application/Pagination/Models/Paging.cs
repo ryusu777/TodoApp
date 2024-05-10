38f8a68d39fdb1065c075ffff5b5fb1928dc66d2
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-11 01:50:08 +0700

    feat IntegrationContext: defined Paging construction logic

--- src/IntegrationContext/IntegrationContext.Application/Pagination/Models/Paging.cs
+++ src/IntegrationContext/IntegrationContext.Application/Pagination/Models/Paging.cs
@@ -1,3 +1,23 @@
 namespace IntegrationContext.Application.Pagination.Models;
 
-public record Paging(int ItemPerPage, int Page);
+public class Paging
+{
+    public int ItemPerPage { get; init; }
+    public int Page { get; init; }
+
+    public Paging() : this(10, 1) { }
+
+    public Paging(int? itemPerPage, int? page)
+    {
+        if (itemPerPage is null || itemPerPage <= 0)
+            itemPerPage = 10;
+
+        if (page is null || page <= 0)
+            page = 1;
+
+        ItemPerPage = (int)itemPerPage;
+        Page = (int)page;
+    }
+
+    public Paging(int page) : this(10, page) { }
+}
