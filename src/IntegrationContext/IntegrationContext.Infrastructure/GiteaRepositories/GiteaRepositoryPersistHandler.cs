ad37e829f68bc9371717834bb9c0aa11d713c323
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-13 04:14:45 +0700

    fix IntegrationContext: persistor db context fix

--- src/IntegrationContext/IntegrationContext.Infrastructure/GiteaRepositories/GiteaRepositoryPersistHandler.cs
+++ src/IntegrationContext/IntegrationContext.Infrastructure/GiteaRepositories/GiteaRepositoryPersistHandler.cs
@@ -7,16 +7,9 @@ namespace IntegrationContext.Infrastructure.GiteaRepositories;
 public class GiteaRepositoryPersistHandler
     : IPersistEventHandler<GiteaRepositoryCreated>
 {
-    private readonly AppDbContext _context;
-
-    public GiteaRepositoryPersistHandler(AppDbContext context)
-    {
-        _context = context;
-    }
-
     public Task Handle(GiteaRepositoryCreated notification, AppDbContext dbContext, CancellationToken cancellationToken)
     {
-        _context.GiteaRepositories.Add(notification.GiteaRepository);
+        dbContext.GiteaRepositories.Add(notification.GiteaRepository);
         return Task.CompletedTask;
     }
 }
