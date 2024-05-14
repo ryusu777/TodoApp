be737365b163682ac45e3660ec42211af043bbdd
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-05-14 11:03:36 +0700

    added CommandOutbox migrations

---
+++ src/IntegrationContext/IntegrationContext.Infrastructure/CommandOutboxes/CommandOutboxesEntityConfiguration.cs
@@ -0,0 +1,41 @@
+using IntegrationContext.Domain.CommandOutboxes;
+using IntegrationContext.Domain.CommandOutboxes.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IntegrationContext.Infrastructure.CommandOutboxes;
+
+public class CommandOutboxesEntityConfiguration : IEntityTypeConfiguration<CommandOutbox>
+{
+    public void Configure(EntityTypeBuilder<CommandOutbox> builder)
+    {
+        builder.ToTable("CommandOutbox");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+            .ValueGeneratedNever()
+            .HasConversion(
+                id => id.Value,
+                value => CommandOutboxId.Create(value)
+            );
+
+        builder.Property(e => e.CommandDetail)
+            .IsRequired(true)
+            .HasColumnType("nvarchar(2000)");
+
+        builder.Property(e => e.CommandResult)
+            .IsRequired(true)
+            .HasColumnType("nvarchar(2000)");
+        
+        builder.Property(e => e.Tries)
+            .IsRequired(true);
+
+        builder.Property(e => e.MaxTries)
+            .IsRequired(true);
+
+        builder.Property(e => e.SuccessAt)
+            .IsRequired(false);
+    }
+}
+
---
+++ src/IntegrationContext/IntegrationContext.Infrastructure/Migrations/20240514040247_AddedCommandOutbox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegrationContext.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCommandOutbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JwtExpiresAt",
+                schema: "integration",
+                table: "GiteaUser");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiresAt",
+                schema: "integration",
+                table: "GiteaUser");
+
+            migrationBuilder.CreateTable(
+                name: "CommandOutbox",
+                schema: "integration",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommandDetail = table.Column<string>(type: "nvarchar(2000)", nullable: false),
+                    CommandResult = table.Column<string>(type: "nvarchar(2000)", nullable: false),
+                    Tries = table.Column<int>(type: "int", nullable: false),
+                    MaxTries = table.Column<int>(type: "int", nullable: false),
+                    SuccessAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommandOutbox", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommandOutbox",
+                schema: "integration");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JwtExpiresAt",
+                schema: "integration",
+                table: "GiteaUser",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiresAt",
+                schema: "integration",
+                table: "GiteaUser",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
